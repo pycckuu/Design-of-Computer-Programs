c556a5afce1225ecfa39a66fcc28f38eec72aeec
Igor <is.markelov@gmail.com>
2013-02-17 17:23:06 -0500

    Making new file with tests for future use

---
+++ test_module.py
@@ -0,0 +1,40 @@
+import time
+
+def c(sequence):
+    """ Generate items in sequence (return them); keeping counts as we go. c.starts is 
+    number of sequences startedl c.item is number of items generated."""
+    
+    c.starts += 1
+    for item in sequence:
+        c.items += 1
+        yield item
+
+def instrument_fn(fn, *args):
+    c.starts, c.items = 0, 0
+    result = fn(*args)
+    print('%s got %s with %5d iters over %7d items'%(
+        fn.__name__, result, c.starts, c.items))
+
+def timedcall(fn, *args):
+    "Call function with args; return the time in seconds and result."
+    t0 = time.clock()
+    result = fn(*args)
+    t1 = time.clock()
+    return t1-t0, result
+
+def average(numbers):
+    "Return the average (arithmetic mean) of a sequence of numbers."
+    return sum(numbers) / float(len(numbers)) 
+
+def timedcalls(n, fn, *args):
+    """Call fn(*args) repeatedly: n times if n is an int, or up to
+    n seconds if n is a float; return the min, avg, and max time"""
+    
+    if isinstance(n, int):
+    	times = [timedcall(fn, *args)[0] for _ in range(n)]
+    else:
+        times = []
+        while sum(times) < n:
+            times.append(timedcall(fn, *args)[0])
+            
+    return  min(times), average(times), max(times)
