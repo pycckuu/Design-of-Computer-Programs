6aec9a364212576ffbff202d0d5b8bd14645e97c
Igor <is.markelov@gmail.com>
2013-02-19 02:49:16 -0500

    improvement of finding palindrome with speed O(N^2)

--- palindrom.py
+++ palindrom.py
@@ -1,19 +1,4 @@
-# --------------
-# User Instructions
-#
-# Write a function, longest_subpalindrome_slice(text) that takes 
-# a string as input and returns the i and j indices that 
-# correspond to the beginning and end indices of the longest 
-# palindrome in the string. 
-#
-# Grading Notes:
-# 
-# You will only be marked correct if your function runs 
-# efficiently enough. We will be measuring efficency by counting
-# the number of times you access each string. That count must be
-# below a certain threshold to be marked correct.
-#
-# Please do not use regular expressions to solve this quiz!
+
 def longest_subpalindrome_slice(text):
     "Return (i, j) such that text[i:j] is the longest palindrome in text."
     if text == '':return(0,0)
